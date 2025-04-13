95ed0e6a0eb7855d255f4c397cd10411b4ad086d
Sumedh Girish <sumedh.girish@gmail.com>
2025-04-13 15:51:35 +0530

    Project Setup

---
+++ build/CMakeFiles/GraphWars.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphWars.dir/link.d"
+  "CMakeFiles/GraphWars.dir/src/main.c.o"
+  "CMakeFiles/GraphWars.dir/src/main.c.o.d"
+  "GraphWars"
+  "GraphWars.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GraphWars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/GraphWars.dir/link.d
@@ -0,0 +1,88 @@
+GraphWars: \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/Scrt1.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crti.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtbeginS.o \
+  CMakeFiles/GraphWars.dir/src/main.c.o \
+  /usr/lib/libraylib.so \
+  /usr/lib/libraylib.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/libc.so \
+  /usr/lib/libc.so \
+  /usr/lib/libc.so \
+  /usr/lib/libc.so.6 \
+  /usr/lib/libc_nonshared.a \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtendS.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crtn.o \
+  /usr/lib/libm.so.6 \
+  /usr/lib/libgcc_s.so.1 \
+  /usr/lib/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/Scrt1.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crti.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtbeginS.o:
+
+CMakeFiles/GraphWars.dir/src/main.c.o:
+
+/usr/lib/libraylib.so:
+
+/usr/lib/libraylib.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/libc.so:
+
+/usr/lib/libc.so:
+
+/usr/lib/libc.so:
+
+/usr/lib/libc.so.6:
+
+/usr/lib/libc_nonshared.a:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtendS.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crtn.o:
+
+/usr/lib/libm.so.6:
+
+/usr/lib/libgcc_s.so.1:
+
+/usr/lib/ld-linux-x86-64.so.2:
