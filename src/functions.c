ad6e65803e3076d4ccb00ee96dfb3ab39e63dea1
Sumedh Girish <sumedh.girish@gmail.com>
2025-04-16 18:14:49 +0530

    Added Shunting Yard Algorithm with Variable Support

--- src/functions.c
+++ src/functions.c
@@ -12,3 +12,90 @@
 // Nice would-haves
 // -> Create a chunking system that only renders functions which are visible
 
+int getPrecedence(char op)
+{
+  switch (op) {
+    case '+': case '-':           return 1;
+    case '*': case '/': case '%': return 2;
+    case '^':                     return 3;
+    default:                      return 0;
+  }
+}
+
+float getOut(char op, float in1, float in2)
+{
+  switch (op) {
+    case '+': return in1 + in2;
+    case '-': return in1 - in2;
+    case '*': return in1 * in2;
+    case '/': return (in2) ? in1 / in2 : 0.0f;
+    case '%': return (in2) ? (int)in1 % (int)in2 : 0.0f;
+    case '^': return powf(in1, in2);
+    default: return 0.0f;
+  }
+}
+
+float evalExpression(Symbol expr[], int n, Var vars[], int m)
+{
+  float operands[n];
+  char operators[n];
+  float* topNum = operands;
+  char* topOp = operators;
+  float result = 0.0f;
+  *topOp = 'b';
+
+  int j;
+  float in1, in2;
+  char currOp;
+  for (int i=0; i<n; ++i) {
+    switch (expr[i].type) {
+      case 'b':   // open brackets
+        *topOp++ = expr[i].data.op;
+        break;
+      case 'B':   // close brackets
+        while (*topOp != 'b') {
+          if (topOp == operators) {
+            return 0.0f;      // if close brace is unmatched
+          }
+          in2 = *topNum--;
+          in1 = *topNum;
+          currOp = *topOp--;
+          *topNum++ = getOut(currOp, in1, in2);
+        }
+        break;
+      case 'n':
+        *topNum++ = expr[i].data.value;
+        break;
+      case 'o':
+        while (getPrecedence(expr[i].data.op) < getPrecedence(*topOp)) {
+          in2 = *topNum--;
+          in1 = *topNum;
+          currOp = *topOp--;
+          *topNum++ = getOut(currOp, in1, in2);
+        }
+        *(++topOp) = expr[i].data.op;
+        break;
+      case 'v':
+        for (j=0; j<m; ++j) {
+          if (vars[j].name == expr[i].data.op) {
+            *topNum++ = vars[j].value;
+            break;
+          }
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  while (topOp != operators) {
+    if (*topOp == 'b') {
+      --topOp;
+    }
+    in2 = *(--topNum);
+    in1 = *(--topNum);
+    currOp = *topOp--;
+    *topNum++ = getOut(currOp, in1, in2);
+  }
+  result = *(--topNum);
+  return result;
+}
--- src/functions.h
+++ src/functions.h
@@ -1,2 +1,18 @@
 #pragma once
 
+typedef struct Var {
+  char name;
+  float value;
+} Var;
+
+typedef struct Symbol {
+  union {
+    float value;
+    char op;
+  } data;
+  char type;
+} Symbol;
+
+int getPrecedence(char op);
+float getOut(char op, float in1, float in2);
+float evalExpression(Symbol expr[], int n, Var vars[], int m);
